cbb374ed3e1001c926fdaaf641fc7345f44066e0
ilhamfzri <ilhamfazri3rd@gmail.com>
2021-11-08 12:48:43 +0700

    add traffic light parameters

--- tvdr/utils/params.py
+++ tvdr/utils/params.py
@@ -10,3 +10,32 @@ class Parameter:
         self.yolo_classes = [2, 3, 5, 7]
         self.yolo_multi_label = False
         self.yolo_max_detection = 500
+
+        # 0 = all area, 1 = traffic light area
+        self.traffic_light_set_view = 0
+
+        # 0 = RGB, 1 = Segmentation
+        self.traffic_light_post_processing = 0
+
+        # traffic ligth area : [x_begin, y_begin, x_end, y_end]
+        self.traffic_light_area = [0, 20, 100, 300]
+
+        self.traffic_light_red_light = {
+            "h_min": 150,
+            "h_max": 180,
+            "s_min": 70,
+            "s_max": 255,
+            "v_min": 50,
+            "v_max": 255,
+            "threshold": 14,
+        }
+
+        self.traffic_light_green_light = {
+            "h_min": 36,
+            "h_max": 70,
+            "s_min": 25,
+            "s_max": 255,
+            "v_min": 25,
+            "v_max": 255,
+            "threshold": 13,
+        }
