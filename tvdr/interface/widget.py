0fdec1536755bcf5424dcf900c877a2c1b9ce773
ilhamfzri <ilhamfazri3rd@gmail.com>
2021-11-13 13:06:07 +0700

    Change title

--- tvdr/interface/widget.py
+++ tvdr/interface/widget.py
@@ -7,9 +7,9 @@ class Widget(QtWidgets.QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Traffic Violation Detection and Recognition")
+        self.setWindowTitle("UGM 2021 : Traffic Violation Detection and Recognition")
         self.menuBar = MenuBar()
         self.mainLayout = QtWidgets.QVBoxLayout()
         self.mainLayout.addLayout(self.menuBar)
-
+        self.mainLayout.setSizeConstraint(QtWidgets.QLayout.SetFixedSize)
         self.setLayout(self.mainLayout)
