b6b7260f8e5a3f0ba36fa376e3907409314a160e
ilhamfzri <ilhamfazri3rd@gmail.com>
2021-11-30 15:49:56 +0700

    add cart2pol and pol2cart formula

--- tvdr/core/algorithm.py
+++ tvdr/core/algorithm.py
@@ -121,3 +121,15 @@ def is_line_intersection_polygon(line, polygon):
             return True
 
     return False
+
+
+def cart2pol(x, y):
+    rho = np.sqrt(x ** 2 + y ** 2)
+    phi = np.arctan2(y, x)
+    return (rho, phi)
+
+
+def pol2cart(rho, phi):
+    x = rho * np.cos(phi)
+    y = rho * np.sin(phi)
+    return (x, y)
