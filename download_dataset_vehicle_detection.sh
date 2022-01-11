8510b35e233b0405acb8179f06b8a942bac71234
ilhamfzri <ilhamfazri3rd@gmail.com>
2022-01-11 09:42:22 +0700

    rename

--- download_dataset_vehicle_detection.sh
+++ download_dataset_vehicle_detection.sh
@@ -3,9 +3,9 @@ mkdir Vehicle_Detection_Dataset
 mkdir Vehicle_Detection_Dataset/images
 mkdir Vehicle_Detection_Dataset/images/train
 mkdir Vehicle_Detection_Dataset/images/val
-mkdir Vehicle_Detection_Dataset/annotations
-mkdir Vehicle_Detection_Dataset/annotations/train
-mkdir Vehicle_Detection_Dataset/annotations/val
+mkdir Vehicle_Detection_Dataset/labels
+mkdir Vehicle_Detection_Dataset/labels/train
+mkdir Vehicle_Detection_Dataset/labels/val
 
 #Download Kitti Dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1R1sW4Yhh2kQYDbxW_fV3N3maKYfJjh4Q' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1R1sW4Yhh2kQYDbxW_fV3N3maKYfJjh4Q" -O kitti_train_images.tar && rm -rf /tmp/cookies.txt
@@ -22,8 +22,8 @@ tar -xvf "kitti_val_yolov5_annotations.tar"
 #Move Kitti Dataset to Vehicle Detection Dataset Folder
 mv -v kitti_train_images/* Vehicle_Detection_Dataset/images/train
 mv -v kitti_val_images/* Vehicle_Detection_Dataset/images/val
-mv -v kitti_train_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/train
-mv -v kitti_val_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/val
+mv -v kitti_train_yolov5_annotations/* Vehicle_Detection_Dataset/labels/train
+mv -v kitti_val_yolov5_annotations/* Vehicle_Detection_Dataset/labels/val
 
 #Download COCO Dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1-3wORzOBNTDEh2Ih9xM0MsRzWy_TXoS3' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1-3wORzOBNTDEh2Ih9xM0MsRzWy_TXoS3" -O coco_train_images.tar && rm -rf /tmp/cookies.txt
@@ -40,8 +40,8 @@ tar -xvf "coco_val_yolov5_annotations.tar"
 #Move COCO Dataset to Vehicle Detection Dataset Folder
 mv -v coco_train_images/* Vehicle_Detection_Dataset/images/train
 mv -v coco_val_images/* Vehicle_Detection_Dataset/images/val
-mv -v coco_train_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/train
-mv -v coco_val_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/val
+mv -v coco_train_yolov5_annotations/* Vehicle_Detection_Dataset/labels/train
+mv -v coco_val_yolov5_annotations/* Vehicle_Detection_Dataset/labels/val
 
 #Download GTA V Dataset
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sfmQpoQPZTJTsoaALrIrl_ok9gZFHAac' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1sfmQpoQPZTJTsoaALrIrl_ok9gZFHAac" -O gtav_train_images.tar && rm -rf /tmp/cookies.txt
@@ -58,8 +58,8 @@ tar -xvf "gtav_val_yolov5_annotations.tar"
 #Move GTA V Dataset to Vehicle Detection Dataset Folder
 mv -v gtav_train_images/* Vehicle_Detection_Dataset/images/train
 mv -v gtav_val_images/* Vehicle_Detection_Dataset/images/val
-mv -v gtav_train_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/train
-mv -v gtav_val_yolov5_annotations/* Vehicle_Detection_Dataset/annotations/val
+mv -v gtav_train_yolov5_annotations/* Vehicle_Detection_Dataset/labels/train
+mv -v gtav_val_yolov5_annotations/* Vehicle_Detection_Dataset/labels/val
 
 #Remove Folder
 rm -r kitti_train_images kitti_val_images coco_train_images coco_val_images gtav_train_images gtav_val_images
