0741cdf682f336178b84f450b01b1e4d529b0337
ilhamfzri <ilhamfazri3rd@gmail.com>
2022-01-30 15:51:07 +0700

    Create download_model.sh

---
+++ download_model.sh
@@ -0,0 +1,2 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1aSNm1rIhUwSBYHwt-jw4pRm4DdKScIui' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1aSNm1rIhUwSBYHwt-jw4pRm4DdKScIui" -O model_v1.tar && rm -rf /tmp/cookies.txt
+tar -xvf model_v1.tar
