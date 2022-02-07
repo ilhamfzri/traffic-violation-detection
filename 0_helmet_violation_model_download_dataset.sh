bce2c07d4ddf6ba466b0f9f5b89a520bb6944d43
ilhamfzri <ilhamfazri3rd@gmail.com>
2022-02-08 03:07:47 +0700

    Create 0_helmet_violation_model_download_dataset.sh

---
+++ 0_helmet_violation_model_download_dataset.sh
@@ -0,0 +1,3 @@
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=16LLBPKi0COHOHljP8lTREs3IpLfFE8tf' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=16LLBPKi0COHOHljP8lTREs3IpLfFE8tf" -O helmet_violation_dataset.tar && rm -rf /tmp/cookies.txt
+tar -xvf helmet_violation_dataset.tar
+mv final_helmet_violation_dataset dataset/helmet_violation_dataset/final_helmet_violation_dataset
