cd848ace298756cb1e6bf5b728b4e12c8c88d3a6
ilhamfzri <ilhamfazri3rd@gmail.com>
2022-02-28 21:58:20 +0700

    chore: add yolov5s config & move yolov5m config

---
+++ configs/vehicle_detection/yolov5s_scratch/train.sh
@@ -0,0 +1,10 @@
+python yolov5/train.py --img 640 \
+--cfg cconfigs/vehicle_detection/yolov5s_scratch/vehicle_detection_model_conf.yaml \
+--hyp configs/vehicle_d etection/yolov5s_scratch/vehicle_detection_hyper_conf.yaml \
+--batch 64 \
+--weight '' \
+--epochs 100 \
+--data configs/vehicle_detection/yolov5s_scratch/vehicle_detection_data_conf.yaml \
+--workers 8 \
+--project "/content/drive/MyDrive/Skripsi/Model Vehicle Detection" \
+--save-period 5
